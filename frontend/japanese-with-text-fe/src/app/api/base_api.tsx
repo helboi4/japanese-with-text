767c06c926f23c3828a41d4cfc93075b7205d74f
helboi4 <h.hopecarter@gmail.com>
2025-10-22 17:37:03 +0100

    Fix duplicate base api files

--- frontend/japanese-with-text-fe/src/app/api/base_api.tsx
+++ frontend/japanese-with-text-fe/src/app/api/base_api.tsx
@@ -5,14 +5,9 @@ class BaseApi {
 		this.base_url = process.env.API_URL ? process.env.API_URL : "http://localhost:8000"
 	}
 
-	async do_request(url: string, options: Request | null, message: string) {
+	async do_request(request: Request | null, message: string) {
 		try {
-			let response;
-			if (options !== null) {
-				response = await fetch(url, options)
-			} else {
-				response = await fetch(url)
-			}
+			const response = await fetch(request)
 			if (!response.ok) {
 				throw new Error(`${message}: ${response.statusText}`)
 			}
